58c50b653b9d5a36f23a58cbcc811d2ff053b401
invitejay <sksskdi122345@gmail.com>
2021-12-15 23:47:22 +0900

    init hilt practice project

---
+++ app/src/main/java/com/kwon/hiltpractice/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.kwon.hiltpractice
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import com.kwon.hiltpractice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
